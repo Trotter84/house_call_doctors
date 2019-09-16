f264108bb6c1b94b2bfee4ac22177375721a731b
Daniel Trotter <flare84@gmail.com>
2019-09-16 17:45:19 -0600

    finished converting frontPageImage CSS

--- client/src/components/coolStuffWorking/FrontPageImage.js
+++ client/src/components/coolStuffWorking/FrontPageImage.js
@@ -7,35 +7,22 @@ import MarkFrontPage2 from '../../images/MarkFrontPage1.jpg';
 import MarkFrontPage3 from '../../images/MarkFrontPage3.jpg';
 
 
-const CarouselContainer = Styled.div`
-  max-height: 600px;
-  max-width: 1166px;
-  padding: 0 0 100px 80px;
-
-`
-
-const Photo = Styled.div`
-  max-height: 600px;
-  max-width: 1166px;
-`
-
-
 class FrontPageImage extends Component {
     render() {
         return (
-          <CarouselContainer className='CarouselContainer'>
-            <Carousel autoPlay interval={4000} showThumbs={false} infiniteLoop>
-                <Photo className='FrontPagePhoto'>
+          <div id='frontPageImageCarouselContainer' className='CarouselContainer'>
+            <Carousel id='frontPageImageCarousel' autoPlay interval={4000} showThumbs={false} infiniteLoop>
+                <div className='frontPageImagePhoto'>
                   <img src={MarkFrontPage1} alt="Mark and Client" />
-                </Photo>
-                <Photo className='FrontPagePhoto'>
+                </div>
+                <div className='frontPageImagePhoto'>
                   <img src={MarkFrontPage2} alt="Mark checking Client's heart beat" />
-                </Photo>
-                <Photo className='FrontPagePhoto'>
+                </div>
+                <div className='frontPageImagePhoto'>
                   <img src={MarkFrontPage3} alt="Mark and Client laughing" />
-                </Photo>
+                </div>
             </Carousel>
-          </CarouselContainer>
+          </div>
         );
     }
 };
