1c00cb7440333ff3c68ea184a73d0b090521374b
Daniel Trotter <flare84@gmail.com>
2019-09-19 16:36:59 -0600

    cleaned up some code

--- client/src/components/pages/OurServices.js
+++ client/src/components/pages/OurServices.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Styled from 'styled-components';
 import { Image, Divider } from 'semantic-ui-react';
 import HCDpageDivider from '../../images/HCDpageDivider.jpg';
 
