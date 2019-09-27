22b096386234b3309cc5e66497fb4407b50d8d3e
Daniel Trotter <flare84@gmail.com>
2019-09-27 16:32:19 -0600

    save point for the bottom menu on mobile

--- client/src/components/menus/BottomMenuMobile.js
+++ client/src/components/menus/BottomMenuMobile.js
@@ -7,7 +7,7 @@ import { Icon } from 'semantic-ui-react';
 const Icons = Styled(Icon)`
   transition: all 200ms ease-in-out;
   &:hover,&.active {
-    color: #2b3d4f;
+    color: #AF3120;
   }
 `
 
@@ -17,37 +17,37 @@ class BottomMenuMobile extends Component {
     return(
       <div id='bottomMenuMainContainer'>
         <Link to={'/home'} className='iconContainers'>
-          <Icons className='icons' size='large' name="home">
+          <Icons className='icons' size='' name="home">
             <p className='bottomMenuText'>Home</p>
           </Icons>
         </Link>
         <div className='vl' />
         <Link to={'/about-us'} className='iconContainers'>
-          <Icons className='icons' size='large' name="!!!!!!">
+          <Icons className='icons' size='' name="user">
             <p className='bottomMenuText'>About Us</p>
           </Icons>
         </Link>
         <div className='vl' />
         <Link to={'/our-providers'} className='iconContainers'>
-          <Icons className='icons' size='large' name="user">
+          <Icons className='icons' size='' name="user med">
             <p className='bottomMenuText'>Providers</p>
           </Icons>
         </Link>
         <div className='vl' />
         <Link to={'/our-services'} className='iconContainers'>
-          <Icons className='icons' size='large' name="!!!!!">
+          <Icons className='icons' size='' name="!!!!!">
             <p className='bottomMenuText'>Services</p>
           </Icons>
         </Link>
         <div className='vl' />
         <Link to={'/our-providers'} className='iconContainers'>
-          <Icons className='icons' size='large' name="!!!!!!">
+          <Icons className='icons' size='' name="!!!!!!">
             <p className='bottomMenuText'>Privacy Policy</p>
           </Icons>
         </Link>
         <div className='vl' />
         <Link to={'/contact-us'} className='iconContainers'>
-          <Icons className='icons' size='large' name="comments">
+          <Icons className='icons' size='' name="comments">
             <p className='bottomMenuText'>Contact Us</p>
           </Icons>
         </Link>
