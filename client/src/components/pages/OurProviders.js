373f11867a7fa25ec069638db032393f30fc3b1d
Daniel Trotter <flare84@gmail.com>
2019-10-03 20:18:37 -0600

    reworked Our Providers page set up

--- client/src/components/pages/OurProviders.js
+++ client/src/components/pages/OurProviders.js
@@ -22,7 +22,7 @@ const InfoBtn = Styled.a`
 `
 
 const DividerSetLength = Styled(Divider)`
-  width: 527px;
+  width: 100%;
 `
 
 const DividerStateLine = Styled(Divider)`
@@ -54,41 +54,40 @@ class OurProviders extends Component {
             {profiles.map(profile => {
               return(
                 <div id='ourProvidersProfile'>
-                  <Image id='ourProvidersImage' src={profile.image} alt={`Profile Picture of ${profile.shortName}`} />
+                  <img id='ourProvidersImage' src={profile.image} alt={`Profile of ${profile.shortName}`}></img>
                   <h3 id='ourProvidersName'>{profile.name}</h3>
                   <p id='ourProvidersShortBio'> &nbsp; &nbsp; &nbsp; {profile.shortBio}</p>
                   <Modal trigger={<InfoBtn>More Info</InfoBtn>}>
-                    <Modal.Header>{profile.name}</Modal.Header>
-                    <Modal.Content image>
-                      <Image id='ourPrividersInsideImage' wrapped size='medium' src={profile.image} />
-                      <Modal.Description>
-                        <div id='ourProvidersPic'>{profile.bio}<br />{profile.bio2}</div>
-                        <DividerSetLength />
-                        <div id='ourProvidersInfo'>
-                          <div id='ourProvidersEdu'>
-                            <h3>Education and Experience</h3>
-                            <p id='ourProvidersBio'>{profile.edu1}</p>
-                            <p id='ourProvidersBio'>{profile.edu2}</p>
-                            <p id='ourProvidersBio'>{profile.edu3}</p>
-                            <p id='ourProvidersBio'>{profile.edu4}</p>
-                            <p id='ourProvidersBio'>{profile.edu5}</p>
-                            <p id='ourProvidersBio'>{profile.edu6}</p>
-                            <p id='ourProvidersBio'>{profile.edu7}</p>
-                            <p id='ourProvidersBio'>{profile.edu8}</p>
-                          </div>
-                          <div id='ourProvidersInterests'>
-                            <h3>Professional and Personal Interests</h3>
-                            <p id='ourProvidersInt'>{profile.interest1}</p>
-                            <p id='ourProvidersInt'>{profile.interest2}</p>
-                            <p id='ourProvidersInt'>{profile.interest3}</p>
-                            <p id='ourProvidersInt'>{profile.interest4}</p>
-                            <p id='ourProvidersInt'>{profile.interest5}</p>
-                            <p id='ourProvidersInt'>{profile.interest6}</p>
-                            <p id='ourProvidersInt'>{profile.interest7}</p>
-                          </div>
+                    <h3 id='ourProvidersInsideName'>{profile.name}</h3>
+                    <Divider />
+                    <div id='ourProviderContent'>
+                      <img id='ourProvidersInsideImage' src={profile.image} alt={`Profile of ${profile.shortName}`}></img>
+                      <div id='ourProvidersInsideBio'> &nbsp; &nbsp; &nbsp; {profile.bio}<br /><br /> &nbsp; &nbsp; &nbsp; {profile.bio2}</div>
+                      <DividerSetLength />
+                      <div id='ourProvidersInfo'>
+                        <div id='ourProvidersEduContainer'>
+                          <h3 className='ourProvidersInfoTitle'>Education and Experience</h3>
+                          <p className='ourProvidersEdu'>{profile.edu1}</p>
+                          <p className='ourProvidersEdu'>{profile.edu2}</p>
+                          <p className='ourProvidersEdu'>{profile.edu3}</p>
+                          <p className='ourProvidersEdu'>{profile.edu4}</p>
+                          <p className='ourProvidersEdu'>{profile.edu5}</p>
+                          <p className='ourProvidersEdu'>{profile.edu6}</p>
+                          <p className='ourProvidersEdu'>{profile.edu7}</p>
+                          <p className='ourProvidersEdu'>{profile.edu8}</p>
                         </div>
-                      </Modal.Description>
-                    </Modal.Content>
+                        <div id='ourProvidersIntContainer'>
+                          <h3 className='ourProvidersInfoTitle'>Professional and Personal Interests</h3>
+                          <p className='ourProvidersInt'>{profile.interest1}</p>
+                          <p className='ourProvidersInt'>{profile.interest2}</p>
+                          <p className='ourProvidersInt'>{profile.interest3}</p>
+                          <p className='ourProvidersInt'>{profile.interest4}</p>
+                          <p className='ourProvidersInt'>{profile.interest5}</p>
+                          <p className='ourProvidersInt'>{profile.interest6}</p>
+                          <p className='ourProvidersInt'>{profile.interest7}</p>
+                        </div>
+                      </div>
+                    </div>
                   </Modal>
                 </div>
               )
