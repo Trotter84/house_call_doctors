89d18ddcba4212c73b85f065b4a008f4c845a237
Daniel Trotter <flare84@gmail.com>
2019-11-17 03:05:33 -0700

    attempt to fix contact field for some browsers on mobile

--- client/src/components/pages/OurServices.js
+++ client/src/components/pages/OurServices.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Image, Divider } from 'semantic-ui-react';
+import { Image, Divider, Icon } from 'semantic-ui-react';
+import NetlifyForm from 'react-netlify-form';
 import HCDpageDivider from '../../images/HCDpageDivider.jpg';
 import ContactField from '../coolStuffWorking/ContactField';
 
@@ -27,7 +28,59 @@ class OurServices extends Component {
               </div>
             </div>
             <div id='contactFieldMainContainer'>
-              <ContactField />
+
+
+              <NetlifyForm name="Contact" method="POST" data-netlify="true">
+                {({ loading, error, success }) => (
+                  <div>
+                    {loading &&
+                      <div id='contactFieldLoading'>Loading  <Icon loading name='spinner' /></div>
+                    }
+                    {error &&
+                      <div id='contactFieldError'>Your information was not sent. Please try again later.</div>
+                    }
+                    {success &&
+                      <div id='contactFieldSuccess'>Thank you for contacting us!</div>
+                    }
+                    {!loading && !success &&
+
+                      <div id='contactFieldInputField'>
+                        <div id='contactFieldNames'>
+                          <div id='contactFieldFirst'>
+                            <label htmlFor="fname">First Name*</label>
+                            <input type="text" id="fname" name="firstname" required />
+                          </div>
+                          <div id='contactFieldLast'>
+                            <label htmlFor="lname">Last Name*</label>
+                            <input type="text" id="lname" name="lastname" required />
+                          </div>
+                        </div>
+                        <div id='contactFieldEmail'>
+                          <label htmlFor="email">Email*</label>
+                          <input type="email" id="email" name="email" placeholder="example@domain.com" required />
+                        </div>
+                        <div id='contactFieldPhone'>
+                          <label htmlFor="phone">Phone Number</label>
+                          <input type="tel" id="phone" name="phone" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" placeholder="1233456789" />
+                        </div>
+                        <div id='contactFieldMessage'>
+                          <label htmlFor="subject">Message*</label>
+                          <textarea id="subject" name="subject" placeholder="How can we help?.." required></textarea>
+                        </div>
+                        <div id='contactFieldSubmit'>
+                          <button type="submit">Send</button>
+                        </div>
+                        <p id='contactFieldRequired'>* required</p>
+                      </div>
+                    }
+                  </div>
+                )}
+              </NetlifyForm>
+            )
+          }
+        }
+
+
             </div>
           </div>
           <div id='servicesQuarterWhiteSpace' />
