850b44a80b62b1df3ecbc3c61ea2d60e92c270c1
Daniel Trotter <flare84@gmail.com>
2022-11-13 19:23:14 -0700

    added aria-current to mobile nav

--- client/src/components/menus/BottomMenuMobile.js
+++ client/src/components/menus/BottomMenuMobile.js
@@ -16,37 +16,37 @@ class BottomMenuMobile extends Component {
   render() {
     return(
       <div id='bottomMenuMainContainer'>
-        <Link to={'/home'} className='iconContainers'>
+        <Link to={'/home'} aria-current='page' className='iconContainers'>
           <Icons className='icons' name="home">
             <p className='bottomMenuText'>Home</p>
           </Icons>
         </Link>
         <div className='vl' />
-        <Link to={'/about-us'} className='iconContainers'>
+        <Link to={'/about-us'} aria-current='page' className='iconContainers'>
           <Icons className='icons' name="users">
             <p className='bottomMenuText'>About Us</p>
           </Icons>
         </Link>
         <div className='vl' />
-        <Link to={'/our-providers'} className='iconContainers'>
+        <Link to={'/our-providers'} aria-current='page' className='iconContainers'>
           <Icons className='icons' name="user md">
             <p className='bottomMenuText'>Providers</p>
           </Icons>
         </Link>
         <div className='vl' />
-        <Link to={'/our-services'} className='iconContainers'>
+        <Link to={'/our-services'} aria-current='page' className='iconContainers'>
           <Icons className='icons' name="medkit">
             <p className='bottomMenuText'>Services</p>
           </Icons>
         </Link>
         <div className='vl' />
-        <Link to={'/our-privacy-policy'} className='iconContainers'>
+        <Link to={'/our-privacy-policy'} aria-current='page' className='iconContainers'>
           <Icons className='icons' name="lock">
             <p className='bottomMenuText'>Privacy Policy</p>
           </Icons>
         </Link>
         <div className='vl' />
-        <Link to={'/contact-us'} className='iconContainers'>
+        <Link to={'/contact-us'} aria-current='page' className='iconContainers'>
           <Icons className='icons' size='' name="comments">
             <p className='bottomMenuText'>Contact Us</p>
           </Icons>
